1b2d2942e144a4a03b5f9df6ff10b67ad326bf36
Dika Afty <dikaafty@gmail.com>
2025-01-02 14:51:13 +0700

    Select dialog element

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Selectors
 const user = document.querySelector(".user-container");
 const info = document.querySelector(".information");
+const dialog = document.querySelector("dialog");
 const addBtn = document.querySelector(".add-btn");
 
 // State
