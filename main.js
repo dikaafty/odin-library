6169699e01f716fdd348d8f8b0a579a774eaa0d2
Dika Afty <dikaafty@gmail.com>
2025-01-03 14:17:25 +0700

    Select dialog inputs

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const dialog = document.querySelector("dialog");
 const addBtn = document.querySelector(".add-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const confirmBtn = document.querySelector(".confirm-btn");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const totalPagesInput = document.getElementById("total-pages");
+const completedPagesInput = document.getElementById("completed-pages");
 
 // State
 
